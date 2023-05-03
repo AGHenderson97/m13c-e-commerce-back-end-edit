7b4c658918ae122b8a3fd6249de25145d954f550
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-05-02 23:05:13 -0400

    Update db.mjs environment variables for database

--- db/db.mjs
+++ db/db.mjs
@@ -2,9 +2,9 @@ import mysql from 'mysql';
 
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'm13c_username',
-  password: 'm13c_password',
-  database: 'm13c_database'
+  user: process.env.m13c_username,
+  password: process.env.m13c_password,
+  database: process.env.m13c_database
 });
 
 export default pool;
