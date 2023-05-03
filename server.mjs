87abe9103c1b02ae3718b3550f55b3be8f6203ca
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-05-02 22:56:33 -0400

    fixed import statement

--- server.mjs
+++ server.mjs
@@ -1,4 +1,4 @@
-import pool from './db.mjs';
+import pool from './db/db.mjs';
 import express from 'express';
 import routes from './routes/index.mjs';
 import sequelize from './config/connection.mjs';
