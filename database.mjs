add42c381dcfd2af8d0cf71ad5a4a21b764d7639
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-05-03 00:46:49 -0400

    edit

--- database.mjs
+++ database.mjs
@@ -1,8 +1,5 @@
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize('sqlite::memory:', {
-  dialect: 'sqlite',
-  logging: false,
+const sequelize = new Sequelize(process.env.m13c_database, process.env.DB_USER, process.env.DB_PW, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+  port: process.env.DB_PORT,
 });
-
-export default sequelize;
