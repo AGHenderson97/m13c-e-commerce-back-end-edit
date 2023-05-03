f583e3d337e16c8139ea5dca0dce42f1c6f50fd9
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-05-02 21:41:06 -0400

    added code - update a category

--- routes/api/category-routes.mjs
+++ routes/api/category-routes.mjs
@@ -34,3 +34,12 @@ router.post('/', async (req, res) => {
   try {
     const newCategory = await Category.create(req.body);
     res.status(200).json(new
+
+// Update a category
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedCategory = await Category.update(req.body, {
+      where: {
+        id: req.params,
+      },
+    });
