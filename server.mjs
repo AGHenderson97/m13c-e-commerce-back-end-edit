1cc9b151bb76bf6c837920d311af7088db017176
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-05-02 21:04:37 -0400

    Initialized Express server and setup routes

---
+++ server.mjs
@@ -0,0 +1,15 @@
+import express from 'express';
+import routes from './routes/index.mjs';
+import sequelize from './config/connection.mjs';
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening on port: ${PORT}'));
+});
