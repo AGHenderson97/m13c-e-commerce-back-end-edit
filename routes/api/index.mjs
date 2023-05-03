daab10db617035890ef055bfdc5490bbbc4f6033
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-05-03 00:07:49 -0400

    updated code

--- routes/api/index.mjs
+++ routes/api/index.mjs
@@ -1,13 +1,8 @@
-import pool from '../../db/db.mjs';
-import express from 'express';
+import { Router } from 'express';
 import categoryRoutes from './category-routes.mjs';
-import productRoutes from './product-routes.mjs';
-import tagRoutes from './tag-routes.mjs';
 
-const router = express.Router();
+const router = Router();
 
 router.use('/categories', categoryRoutes);
-router.use('/products', productRoutes);
-router.use('/tags', tagRoutes);
 
 export default router;
--- server.mjs
+++ server.mjs
@@ -1,27 +1,12 @@
 import express from 'express';
-import apiRoutes from './routes/api/index.mjs';
-import connection from './config/connection.mjs';
+import routes from './routes/api/index.mjs';
 
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-// Middleware for JSON parsing
 app.use(express.json());
-
-// Middleware for URL encoded data
 app.use(express.urlencoded({ extended: true }));
 
-// Routes
-app.use('/api', apiRoutes);
+app.use('/api', routes);
 
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
-});
-
-// Connection listener
-connection.on('connect', () => {
-  app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-  });
-});
+app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
