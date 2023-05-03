10921fd6e124f924350fd426edcf30fa8f1f2400
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-05-02 21:44:40 -0400

    fixed syntax, update method, add export statement

--- routes/api/category-routes.mjs
+++ routes/api/category-routes.mjs
@@ -33,13 +33,26 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   try {
     const newCategory = await Category.create(req.body);
-    res.status(200).json(new
+    res.status(200).json(newCategory);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 // Update a category
 router.put('/:id', async (req, res) => {
   try {
     const updatedCategory = await Category.update(req.body, {
       where: {
-        id: req.params,
+        id: req.params.id,
       },
     });
+    res.json(updatedCategory);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+export default router;
